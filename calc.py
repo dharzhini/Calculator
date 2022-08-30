f72dc46d70ceced076429f8f3a51ca1b83be3375
dharzhini <101085064+dharzhini@users.noreply.github.com>
2022-08-30 14:50:17 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,54 @@
+a=int(input("Enter first num"))
+b=input("what do you want to do? +,-,*,/,%,^,//")
+c=int(input("enter your second number"))
+e=input("what do you want to do? +,-,*,/,%,^,//")
+f=int(input("enter your third number"))
+if b=="+":
+    d=a+c
+
+if b=="-":
+    d=a-c
+
+if b=="*":
+    d=a*c
+
+if b=="/":
+    d=a/c
+
+if b=="%":
+    d=a%c
+
+if b=="^":
+    d=a^c
+
+if b=="//":
+    d=a//c
+
+if e=="+":
+    g=d+f
+
+if e=="-":
+    g=d-f
+
+if e=="*":
+    g=d*f
+
+if e=="/":
+    g=d/f
+
+if e=="%":
+    g=d%f
+
+if e=="^":
+    g=d^f
+
+if e=="//":
+    g=d//f
+
+print(g)
+
+
+
+
+
+
