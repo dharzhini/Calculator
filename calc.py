e560392db001adb8bad51fd2dbf65b3da5a67e54
dharzhini <101085064+dharzhini@users.noreply.github.com>
2022-08-30 14:55:24 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+#simple calculator by priya
 a=int(input("Enter first num"))
 b=input("what do you want to do? +,-,*,/,%,^,//")
 c=int(input("enter your second number"))
@@ -5,50 +6,31 @@ e=input("what do you want to do? +,-,*,/,%,^,//")
 f=int(input("enter your third number"))
 if b=="+":
     d=a+c
-
 if b=="-":
     d=a-c
-
 if b=="*":
     d=a*c
-
 if b=="/":
     d=a/c
-
 if b=="%":
     d=a%c
-
 if b=="^":
     d=a^c
-
 if b=="//":
     d=a//c
-
 if e=="+":
     g=d+f
-
 if e=="-":
     g=d-f
-
 if e=="*":
     g=d*f
-
 if e=="/":
     g=d/f
-
 if e=="%":
     g=d%f
-
 if e=="^":
     g=d^f
-
 if e=="//":
     g=d//f
-
 print(g)
-
-
-
-
-
-
+#thankyou,goodbye:)
